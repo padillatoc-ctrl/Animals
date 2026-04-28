42bd56426748749d768c4ee41edbbf146e2f0eef
padillatoc-ctrl <padillatoc@gmail.com>
2026-04-28 07:16:18 -0600

    Add Animal class with properties and methods

--- Animals.cs
+++ Animals.cs
@@ -1,1 +1,27 @@
+// Animal.cs  –  Clase base (clase padre)
 
+class Animal
+{
+    // Atributos (propiedades)
+    public string Nombre;
+    public int Edad;
+
+    // Constructor
+    public Animal(string nombre, int edad)
+    {
+        Nombre = nombre;
+        Edad   = edad;
+    }
+
+    // Método
+    public void HacerSonido()
+    {
+        Console.WriteLine(Nombre + " hace un sonido.");
+    }
+
+    // Método para mostrar info
+    public void MostrarInfo()
+    {
+        Console.WriteLine("Nombre: " + Nombre + ", Edad: " + Edad);
+    }
+}
